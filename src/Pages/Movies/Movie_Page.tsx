df53071c134ec921f80df53c6f8d3007f90d6833
Thibault CASTILLON <thibault.castillon@epitech.eu>
2023-06-09 15:01:54 +0200

    [FIX] finish cleaning for unused functions and seperated others in proper files

--- src/Pages/Movies/Movie_Page.tsx
+++ src/Pages/Movies/Movie_Page.tsx
@@ -116,70 +116,9 @@ function Movie_Page(props: any) {
         setOpenPopup(false);
     }
     
-    interface CrewMember {
-        id: String,
-        name: String,
-        job: [String],
-        profile_path: String,
-    }
 
-    const identify_principal = (people: any) => {
-        let principal_cast : CrewMember[] = [];
-        for (let i = 0; i < people.length; i++) {
-            if ((people[i].job === 'Director') || (people[i].job === 'Screenplay') || (people[i].job === 'Writer') || (people[i].job === 'Original Music Composer')) {
-                principal_cast = add_principal(principal_cast, people[i]);
-            }
-        }
-        return principal_cast;
-    }
 
-    const add_principal = (principal_cast: CrewMember[], person: any) => {
-        let is_present = false;
-        if (principal_cast.length === 0) {
-            principal_cast.push({
-                id: person.id,
-                name: person.name,
-                job: [person.job],
-                profile_path: person.profile_path,
-            });
-        } else {
-            for (let i = 0; i < principal_cast.length; i++) {
-                if (principal_cast[i].id === person.id) {
-                    principal_cast[i].job.push(person.job);
-                    is_present = true;
-                }
-            }
-            if (!is_present) {
-                principal_cast.push({
-                    id: person.id,
-                    name: person.name,
-                    job: [person.job],
-                    profile_path: person.profile_path,
-                });
-            }
-        }
-        return principal_cast;
-    }
 
-    interface Actor {
-        id: String,
-        name: String,
-        character: String,
-        profile_path: String,
-    }
-
-    const identify_actor = (people: any) => {
-        let principal_cast : Actor[] = [];
-        for (let i = 0; i < people.length; i++) {
-            principal_cast.push({
-                id: people[i].id,
-                name: people[i].name,
-                character: people[i].character,
-                profile_path: people[i].profile_path,
-            });
-        }
-        return principal_cast;
-    }
     
 
     return (
